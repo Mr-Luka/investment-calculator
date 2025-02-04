6e30fccd167c0eea2ffc47bc1d14dac81dc0fb72
Luka <luka.luka.luka93@gmail.com>
2025-02-04 15:09:54 -0800

    lifting the state up

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,5 @@
-export default function Result () {
+export default function Result ({ input }) {
     return (
-        <div className="result">
-            <thead>
-                <th>Year</th>
-                <th>Investment Value</th>
-                <th>Interest (year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
-            </thead>
-        </div>
+        <p>Results....</p>
     )
 }
